88cc0f75fd608896fa0836d4fa7a892dc154912f
Tyuiu.GutsuAV@outlook.com <gutsu05@mail.ru>
2023-10-14 22:44:39 +0300

    Task1

---
+++ Tyuiu.GutsuAV.Sprint1.Task1.V26.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+using Tyuiu.GutsuAV.Sprint1.Task1.V26.Lib;
+
+
+namespace Tyuiu.GutsuAV.Sprint1.Task1.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+
+            DataService ds = new DataService();
+            double x = 2.0;
+            double y = 3.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(2, res);
+
+        }
+    }
+}
